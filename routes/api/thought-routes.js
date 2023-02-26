3c0ff85f8646f2f8edd5e4f11f7bb2a8ab48d628
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-02-25 16:24:28 -0800

    added logic to get, update, and delete thoughts. Almost same as from user

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,20 +3,71 @@ const { Thought, Reaction } = require("../../models");
 
 //TODO: ROUTE TO GET ALL THOUGHTS
 router.get("/", (req, res) => {
-  res.status(200).send("Hello World 2");
+  Thought.find({}, (err, thoughts) => {
+    if (err) {
+      res.status(400).json(err);
+    } else {
+      res.status(200).json(thoughts);
+    }
+  });
 });
 
 //TODO: ROUTE TO CREATE A NEW THOUGHT
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  Thought.create(
+    {
+      thoughtText: req.body.thoughtText,
+      username: req.body.username,
+    },
+    (err, thought) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json(thought);
+      }
+    }
+  );
+});
 
 //TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
-router.get("/:thoughtId", (req, res) => {});
+router.get("/:thoughtId", (req, res) => {
+  Thought.findOne({ _id: req.params.thoughtId }, (err, thought) => {
+    if (err) {
+      res.status(400).json(err);
+    } else {
+      res.status(200).json(thought);
+    }
+  }).populate("reactions");
+});
 
 //TODO: ROUTE TO UPDATE A THOUGHT
-router.put("/", (req, res) => {});
+router.put("/", (req, res) => {
+  Thought.findOneAndUpdate(
+    { _id: req.params.thoughtId },
+    {
+      thoughtText: req.body.thoughtText,
+      username: req.body.username,
+    },
+    (err, thought) => {
+      if (err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json(thought);
+      }
+    }
+  );
+});
 
 //TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
-router.delete("/:thoughtId", (req, res) => {});
+router.delete("/:thoughtId", (req, res) => {
+  Thought.findOneAndDelete({ _id: req.params.thoughtId }, (err, thought) => {
+    if (err) {
+      res.status(400).json(err);
+    } else {
+      res.status(200).json(thought);
+    }
+  });
+});
 
 //TODO: ROUTE TO ADD REACTION TO A THOUGHT
 router.post("/:thoughtId/reactions", (req, res) => {});
